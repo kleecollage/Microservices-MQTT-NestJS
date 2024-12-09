4a64578a35b215dd281d1a274f5205edd7894dfe
Klee Collage <klee.collage@gmail.com>
2024-12-09 16:54:54 -0600

    refactor(module): Add Publisher and Subscriber modules to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MqttPublisherModule } from 'src/modules/mqtt-publisher/mqtt-publisher.module';
+import { MqttSubscriberModule } from 'src/modules/mqtt-subscriber/mqtt-subscriber.module';
 
 @Module({
-  imports: [],
+  imports: [MqttPublisherModule, MqttSubscriberModule],
   controllers: [],
   providers: [],
 })
